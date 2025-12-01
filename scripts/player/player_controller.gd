82f9d39733c3b1116984d84c232e112de9963b43
ExgleGaming <EaglesFan91x@outlook.com>
2025-11-30 23:13:00 -0500

    initial player controller set

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -2,5 +2,40 @@ class_name PlayerController
 extends CharacterBody3D
 
 
+@export var debug: bool = false
+
+@export_category("References")
+@export var camera: CameraController
+
+@export_category("Movement Settings")
+@export_group("Easing")
+@export var acceleration: float = 0.2
+@export var deceleration: float = 0.5
+
+var _input_dir: Vector2 = Vector2.ZERO
+var _movement_velocity: Vector3 = Vector3.ZERO
+var speed: float = 0.0
+
+
+func _physics_process(delta: float) -> void:
+    if!is_on_floor():
+        velocity += get_gravity() * delta
+
+    _input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+    var current_velocity: Vector2 = Vector2(_movement_velocity.x, _movement_velocity.z)
+    var direction: Vector3 = (transform.basis * Vector3(_input_dir.x, 0, _input_dir.y)).normalized()
+
+    if direction:
+        current_velocity = lerp(current_velocity, Vector2(direction.x, direction.z) * speed, acceleration)
+    else:
+        current_velocity = current_velocity.move_toward(Vector2.ZERO, deceleration)
+
+    _movement_velocity = Vector3(current_velocity.x, velocity.y, current_velocity.y)
+
+    velocity = _movement_velocity
+
+    move_and_slide()
+
+
 func update_rotation(rotation_input: Vector3) -> void:
     global_transform.basis = Basis.from_euler(rotation_input)
