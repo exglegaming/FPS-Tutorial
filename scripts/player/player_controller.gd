9bb0794c95d8f3be22ee47db8c5d2ec0725460cc
ExgleGaming <EaglesFan91x@outlook.com>
2025-12-04 23:49:15 -0500

    camera juice

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -6,6 +6,7 @@ extends CharacterBody3D
 
 @export_category("References")
 @export var camera: CameraController
+@export var camera_effects: CameraEffects
 @export var state_chart: StateChart
 @export var standing_collision: CollisionShape3D
 @export var crouching_collision: CollisionShape3D
@@ -23,27 +24,32 @@ extends CharacterBody3D
 
 @export_category("Jump Settings")
 @export var jump_velocity: float = 5.0
+@export var fall_velocity_threshold: float = -5.0
+
+@export_category("Data Helpers")
+@export var data_relative_velocity: Vector3
 
 var _input_dir: Vector2 = Vector2.ZERO
 var _movement_velocity: Vector3 = Vector3.ZERO
-var sprint_modifier: float = 0.0
-var crouch_modifier: float = 0.0
-var speed: float = 0.0
+var _sprint_modifier: float = 0.0
+var _crouch_modifier: float = 0.0
+var _speed: float = 0.0
+var current_fall_velocity: float
 
 
 func _physics_process(delta: float) -> void:
     if!is_on_floor():
         velocity += get_gravity() * delta
     
-    var speed_modifier = sprint_modifier + crouch_modifier
-    speed = default_speed + speed_modifier
+    var speed_modifier = _sprint_modifier + _crouch_modifier
+    _speed = default_speed + speed_modifier
 
     _input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
     var current_velocity: Vector2 = Vector2(_movement_velocity.x, _movement_velocity.z)
     var direction: Vector3 = (transform.basis * Vector3(_input_dir.x, 0, _input_dir.y)).normalized()
 
     if direction:
-        current_velocity = lerp(current_velocity, Vector2(direction.x, direction.z) * speed, acceleration)
+        current_velocity = lerp(current_velocity, Vector2(direction.x, direction.z) * _speed, acceleration)
     else:
         current_velocity = current_velocity.move_toward(Vector2.ZERO, deceleration)
 
@@ -59,24 +65,33 @@ func update_rotation(rotation_input: Vector3) -> void:
 
 
 func sprint() -> void:
-    sprint_modifier = sprint_speed
+    _sprint_modifier = sprint_speed
 
 
 func walk() -> void:
-    sprint_modifier = 0.0
+    _sprint_modifier = 0.0
 
 
 func stand() -> void:
-    crouch_modifier = 0.0
+    _crouch_modifier = 0.0
     standing_collision.disabled = false
     crouching_collision.disabled = true
 
 
 func crouch() -> void:
-    crouch_modifier = crouch_speed
+    _crouch_modifier = crouch_speed
     standing_collision.disabled = true
     crouching_collision.disabled = false
 
 
 func jump() -> void:
     velocity.y += jump_velocity
+
+
+func check_fall_speed() -> bool:
+    if current_fall_velocity < fall_velocity_threshold:
+        current_fall_velocity = 0.0
+        return true
+    else:
+        current_fall_velocity = 0.0
+        return false
